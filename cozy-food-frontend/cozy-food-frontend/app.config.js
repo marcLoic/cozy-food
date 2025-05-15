986c0bfcbb1e0fb521da6cc75e19c95453790203
marcloic <marcloic81@gmail.com>
2025-05-15 13:07:16 +0200

    WIP

--- cozy-food-frontend/cozy-food-frontend/app.config.js
+++ cozy-food-frontend/cozy-food-frontend/app.config.js
@@ -10,7 +10,7 @@ export default {
     scheme: "myapp",
     userInterfaceStyle: "automatic",
     splash: {
-      image: "./assets/images/splash.png",
+      image: "./assets/images/chicken1.png",
       resizeMode: "contain",
       backgroundColor: "#ffffff"
     },
